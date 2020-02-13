aaa6c977b3c852dc500a1d85c69c0d4a9ff907d8
Viktor Zubtsov <mr.viktor.1996@yandex.ru>
2020-02-13 10:21:42 +0700

    Modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,9 @@ let gulp = require('gulp'),
       minify = require('gulp-minify'),
       autoprefixer = require('gulp-autoprefixer'),
       spritesmith = require('gulp.spritesmith'),
-     // concatCss = require('gulp-concat-css'),
       concat = require('gulp-concat');
+
+
 
 //   1. directories
 //   2. dev
@@ -21,27 +22,36 @@ let gulp = require('gulp'),
 
 gulp.task('directories', function () {
   return gulp.src('*.*', {read: false})
-    .pipe(gulp.dest('./src/pages'))/// dist
+    .pipe(gulp.dest('./src/pages'))
     .pipe(gulp.dest('./src/img'))
     .pipe(gulp.dest('./src/img/sprite'))
     .pipe(gulp.dest('./src/fonts'))
     .pipe(gulp.dest('./src/js'))
+    .pipe(gulp.dest('./src/lib'))
     .pipe(gulp.dest('./src/modules'))
     .pipe(gulp.dest('./src/sass'))
     .pipe(gulp.dest('./src/sass/base'))
+    .pipe(gulp.dest('./src/sass/component'))
     .pipe(gulp.dest('./src/sass/layout'));
 });
 
 //  2. dev
 
-
 gulp.task('html', function() {
-  return gulp.src("./src/index.html")
-      .pipe(gulp.dest("./public"));
+  return gulp.src(["./src/*.html", "./src/pages/*.html"])
+    .pipe(debug({title: 'html'}))
+    .pipe(gulp.dest("./public"));
+});
+
+gulp.task('pug', function() {
+  return gulp.src(["./src/index.pug", "./src/pages/*.pug"])
+    .pipe(pug({pretty: '\t'}))  // generate file pug
+    .pipe(pug())
+    .pipe(debug({title: 'build-pug'}))
+    .pipe(gulp.dest("./public"));
 });
 
 sass.compiler = require('node-sass');
-
 gulp.task('sass', function () {
   return gulp.src('./src//sass/**/style.sass')
     .pipe(debug({title: 'src'}))
@@ -68,18 +78,19 @@ gulp.task('fonts', function () {
     .pipe(fontmin({
         text: 'text',
     }))
+    .pipe(debug('fonts'))
     .pipe(gulp.dest('./public/fonts'));
 });
 
 gulp.task('image', function () {
-  return gulp.src('./src/img/*.png', './src/img/*.svg', './src/img/*.jpg')
+  return gulp.src(['./src/img/*.png', './src/img/*.svg', './src/img/*.jpg'])
     .pipe(image())
     .pipe(debug({title: 'image'}))
     .pipe(gulp.dest('./public/img'));
 });
 
 gulp.task('sprite', function() {
-  var spriteData = 
+  let spriteData = 
       gulp.src('./src/img/sprite/*.*') 
           .pipe(spritesmith({
               imgName: 'sprite.png',
@@ -87,10 +98,8 @@ gulp.task('sprite', function() {
               padding: 10,
               imgPath: '../img/sprite.png',
           }));
-
   spriteData.img.pipe(gulp.dest('./public/img')); 
   spriteData.css.pipe(gulp.dest('./src/sass/base')); 
-
   return spriteData;
 });
 
@@ -100,14 +109,25 @@ gulp.task('lib', function copy() {
       .pipe(dest('./public/lib/'));
 });
 
+
+
 gulp.task('relation', gulp.series (
   `clean`,
   gulp.parallel( 'html', `sass`, `js`, 'fonts' , 'image', 'sprite' ,'lib')
 ));
 
+gulp.task('relation+pug', gulp.series (
+  `clean`,
+  gulp.parallel( 'pug', `sass`, `js`, 'fonts' , 'image', 'sprite' ,'lib')
+));
+
+
 gulp.task('watch', function () {
   gulp.watch(`./src/**/index.html`, gulp.series(`html`))
-  // gulp.watch(`./src/modules/*.pug`, gulp.series(`pug`))
+  gulp.watch(`./src/pages/*.html`, gulp.series(`html`))
+  gulp.watch(`./src/**/index.pug`, gulp.series(`pug`))
+  gulp.watch(`./src/pages/*.pug`, gulp.series(`pug`))
+  gulp.watch(`./src/modules/*.pug`, gulp.series(`pug`))
   gulp.watch(`./src/sass/**/*.sass`, gulp.series(`sass`))
   gulp.watch(`./src/js/**/*.js`, gulp.series(`js`))
   gulp.watch(`./src/fonts/**/*.ttf`, gulp.series(`fonts`)) 
@@ -123,16 +143,38 @@ gulp.task('serve', function () {
   browserSync.watch(`../Tesla website/**/*.*`).on(`change`, browserSync.reload);
 });
 
+// basic command
+
 gulp.task('dev',
   gulp.series (`relation`, gulp.parallel(`watch`, 'serve')));
+
+gulp.task('dev+pug',
+  gulp.series (`relation+pug`, gulp.parallel(`watch`, 'serve')));
+
+
+
+
 
 //  3. build
 
 gulp.task('build-pug', function() {
-  return gulp.src("./src/index.pug")
+  return gulp.src(["./src/index.pug", "./src/pages/*.pug"])
     .pipe(pug({pretty: '\t'}))  // generate file pug
     .pipe(pug())
     .pipe(debug({title: 'build-pug'}))
+    .pipe(gulp.dest("./build"));
+});
+
+gulp.task('pages-html', function() {
+  return gulp.src("./src/pages/*.html")
+    .pipe(debug({title: 'build-html'}))
+    .pipe(gulp.dest("./build"));
+});
+
+
+gulp.task('build-html', function() {
+  return gulp.src(["./src/index.html", "./src/pages/*.html"])
+    .pipe(debug({title: 'build-html'}))
     .pipe(gulp.dest("./build"));
 });
 
@@ -203,12 +245,14 @@ gulp.task('build-lib', function copy() {
 
 gulp.task('build', gulp.series (
   `build-clean`,
-  gulp.parallel(`build-pug`, `compress-sass`, 'build-image', 'build-sprite', 'compress-js', 'build-fonts', 'build-lib')
+  gulp.parallel(`build-html`, `compress-sass`, 'build-image', 'build-sprite', 'compress-js', 'build-fonts', 'build-lib')
 ));
 
-// gulp.task('hello', function() {
-//   console.log('Hello Zell+++++++');
-// });
+gulp.task('build+pug', gulp.series (
+  `build-clean`,
+  gulp.parallel(`build-pug`,  `pages-html`, `compress-sass`, 'build-image', 'build-sprite', 'compress-js', 'build-fonts', 'build-lib')
+));
+
 
 
 
