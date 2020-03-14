c9c84885b24413bd9a4feb90f7335acf61a4cb42
Viktor Zubtsov <mr.viktor.1996@yandex.ru>
2020-03-14 08:37:57 +0700

    Changes to the page

--- src/js/menu-active.js
+++ src/js/menu-active.js
@@ -1,22 +1,9 @@
-// function menu () {
-//     let menuIcon =  document.querySelector('.menu-icon'),
-//         nav = document.querySelector('.navigation');
-
-//     return menuIcon.onclick = function(){
-//         menuIcon.classList.toggle('menu-icon-active');
-//         nav.classList.toggle('navigation_active');
-//     }
-// }
-
-
-// menu ();
-
 (function () {
     let menuIcon =  document.querySelector('.menu-icon'),
     nav = document.querySelector('.navigation');
 
     return menuIcon.onclick = function(){
-        menuIcon.classList.toggle('menu-icon-active');
+        menuIcon.classList.toggle('menu-icon_active');
         nav.classList.toggle('navigation_active');
     }
 }());
